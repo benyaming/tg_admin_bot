3345e74ca34129a5e3c542205e664910c84b0a57
Benyomin Ginzburg <benyomin.94@gmail.com>
2019-09-02 22:55:58 +0300

    ban user after 5 minutes

--- bot.py
+++ bot.py
@@ -1,29 +1,55 @@
 from os import environ
+from asyncio import sleep, create_task
 
+from aioredis import create_redis
 from aiogram import Dispatcher, Bot
 from aiogram.utils.executor import start_polling
 from aiogram.types import ContentTypes, Message, InlineKeyboardButton, \
     InlineKeyboardMarkup, ChatPermissions, CallbackQuery
 
 
-question_text = 'А не спамер ли ты?'
+question_text = 'Добро пожаловать! У вас есть 5 минут чтобы доказать, что вы не спамер!'
 button_text = 'Я не спамер!'
-answer_query_right_user = 'Добро пожаловать'
+answer_query_right_user = 'Добро пожаловать!'
 answer_query_wrong_user = 'Эта кнопка не для тебя!'
 
-bot = Bot('908972444:AAFpq6Odobjf_DaX4zHfwCx00RQH4M1SQ6c')
+bot = Bot(environ.get('TOKEN'))
 dp = Dispatcher(bot)
+
+
+async def init_user_track(user_token: str) -> None:
+    redis = await create_redis(
+        f'redis://{environ.get("REDIS_HOST")}:{environ.get("REDIS_PORT")}'
+    )
+    await redis.set(user_token, 1)
+
+    await sleep(60)
+    user_in_redis = await redis.exists(user_token)
+    if not bool(user_in_redis):
+        return
+    user, chat = user_token.split(':')
+    await bot.kick_chat_member(chat, user)
+    await redis.delete(user_token)
+
+
+async def stop_user_track(user_token: str) -> None:
+    redis = await create_redis(
+        f'redis://{environ.get("REDIS_HOST")}:{environ.get("REDIS_PORT")}'
+    )
+    redis.delete(user_token)
 
 
 @dp.message_handler(content_types=ContentTypes.NEW_CHAT_MEMBERS)
 async def new_chat_member(msg: Message):
+    print(123)
     rights = ChatPermissions(
         can_send_messages=False,
         can_send_media_messages=False,
         can_send_other_messages=False
     )
-    await bot.restrict_chat_member(msg.chat.id, msg.from_user.id, permissions=rights)
-
+    await bot.restrict_chat_member(msg.chat.id, msg.new_chat_members[0].id, permissions=rights)
+    create_task(init_user_track(f'{msg.from_user.id}:{msg.chat.id}'))
+    # await init_user_track(f'{msg.from_user.id}:{msg.chat.id}')
     answer = await bot.send_message(msg.chat.id, question_text)
 
     kb = InlineKeyboardMarkup()
@@ -31,7 +57,6 @@ async def new_chat_member(msg: Message):
         button_text,
         callback_data=f'{msg.chat.id}:{msg.from_user.id}:{answer.message_id}')
     )
-
     await bot.edit_message_reply_markup(msg.chat.id, answer.message_id, reply_markup=kb)
 
 
@@ -47,12 +72,12 @@ async def handle_button(call: CallbackQuery):
 
     if call.from_user.id == user:
         answer = answer_query_right_user
-
         await bot.restrict_chat_member(chat_id, user, permissions=rights)
         await bot.delete_message(chat_id, msg_id)
     else:
         answer = answer_query_wrong_user
     await call.answer(answer)
+    await stop_user_track(f'{call.from_user.id}:{call.message.chat.id}')
 
 
-start_polling(dp)
+start_polling(dp, skip_updates=True)
